a0b4dfbde922aed78caf8577f9bebafe3afd68c4
h m tareq <prantaakhond159@gmail.com>
2022-12-22 15:50:20 +0600

    Typewriter add

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Typewriter from 'typewriter-effect';
 
 const Banner = () => {
     return (
@@ -7,8 +8,16 @@ const Banner = () => {
         text-center text-white ">
             <div className="hero-overlay bg-opacity-80"></div>
             <div>
-                <h1 className='text-7xl font-bold leading-normal'>MD HASAN MIAH</h1>
-                <p className='text-3xl mb-3'>I am a <span className='text-[#ff014f]'> Front End Developer.</span></p>
+                <h1 className='text-7xl font-bold leading-normal'>MD HASAN MIA</h1>
+                <p className='text-3xl mb-3 font-semibold'>I am a <span className='text-[#ff014f]'>
+                    <Typewriter
+                        options={{
+                            strings: ["Front End Developer", "React Developer", "Full Stact Developer"],
+                            autoStart: true,
+                            loop: true,
+                        }}
+                    />
+                </span></p>
                 <a href='#contact' className='text-[#ff014f] p-3 bg-white rounded text-lg font-medium mt-4 inline-block'>Contact Me</a>
             </div>
         </div>
