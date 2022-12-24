08ddf134ee1b1867a0ae1176543c634144b738ae
h m tareq <prantaakhond159@gmail.com>
2022-12-24 15:04:15 +0600

    Wave ripple finish

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,26 +1,35 @@
 import React from 'react';
 import Typewriter from 'typewriter-effect';
 
+import WaterWave from 'react-water-wave';
+import image from '../assets/IMG20210515175953-01.jpeg';
 const Banner = () => {
     return (
-        <div className="hero min-h-[90vh]
-        bg-[url('/src/assets/IMG20210515175953-01.jpeg')] 
-        text-center text-white ">
-            <div className="hero-overlay bg-opacity-80"></div>
-            <div>
-                <h1 className='text-7xl font-bold leading-normal'>MD HASAN MIA</h1>
-                <p className='text-3xl mb-3 font-semibold'>I am a <span className='text-[#ff014f]'>
-                    <Typewriter
-                        options={{
-                            strings: ["Front End Developer", "React Developer", "Full Stact Developer"],
-                            autoStart: true,
-                            loop: true,
-                        }}
-                    />
-                </span></p>
-                <a href='#contact' className='text-[#ff014f] p-3 bg-white rounded text-lg font-medium mt-4 inline-block'>Contact Me</a>
-            </div>
-        </div>
+        <WaterWave
+            className="h-[90vh] bg-contain"
+            imageUrl={image}
+        >
+            {
+                () => (
+                    <div className=" hero text-center text-white h-full">
+                        <div className="hero-overlay bg-opacity-60"></div>
+                        <div>
+                            <h1 className='text-4xl sm:text-5xl lg:text-7xl font-bold leading-normal'>MD HASAN MIA</h1>
+                            <p className='text-2xl md:text-3xl mb-3 font-semibold'>I am a <span className='text-[#ff014f]'>
+                                <Typewriter
+                                    options={{
+                                        strings: ["Front End Developer", "React Developer", "Full Stact Developer"],
+                                        autoStart: true,
+                                        loop: true,
+                                    }}
+                                />
+                            </span></p>
+                            <a href='#contact' className='text-[#ff014f] p-3 bg-white rounded text-lg font-medium mt-4 inline-block'>Contact Me</a>
+                        </div>
+                    </div>
+                )
+            }
+        </WaterWave>
     );
 };
 
